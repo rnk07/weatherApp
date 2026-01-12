b44d0af3c3b1bc6f6849861250afb35840554a2b
rnk07 <patelrnk07@gmail.com>
2026-01-11 20:41:37 -0500

    added get weather code and made some changes to git ignore

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -1,24 +1,28 @@
 import express from 'express'
 import fetch from 'node-fetch';
 
+
 const router = express.Router();
-const API_KY = "";
+
 
 // GET 
 router.get("/:city", async (req, res) => {
-
+    const API_KEY = process.env.API_KEY;  //Uses api key from .env file
     try {
+        console.log(req.params)
         const { city } = req.params;
         if (!city) {
             return res.status(400).json({ error: "City is required" });
         }
 
+        if (!API_KEY) return res.status(500).json({ error: "API key missing" });
 
-        const url = `http://api.weatherapi.com/v1/current.json?key=${API_KY}&q=${city}&aqi=no`;
+        const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
 
 
         const response = await fetch(url)
         const data = await response.json();
+        console.log(data)
 
         // Send back needed Data
 
@@ -37,7 +41,7 @@ router.get("/:city", async (req, res) => {
     } catch (error) {
 
         console.error(error);
-        res.status(500).json({error:"Server Side Error."})
+        res.status(500).json({ error: "Server Side Error." })
 
     }
 
