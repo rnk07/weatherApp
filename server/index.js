f41d071614f1dd79a9a49f4243f4c707da71833f
rnk07 <patelrnk07@gmail.com>
2026-01-11 20:57:54 -0500

    updated port config

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 import dotenv from "dotenv"
+
 import express from "express"
 import weatherRoutes from "./routes/weather.js"
 
-
 dotenv.config(); //Load .env file
+const PORT = process.env.PORT;
+
 const app = express();
 app.use(express.json()); //Middleware to parse json
-const port = 9000;
 
 // Mount Weather Routes
 app.use("/weather",weatherRoutes);
 
 
 // Sever Start
-app.listen(port, ()=>{
-    console.log(`Server is running on ${port}`);
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);
     
 })
